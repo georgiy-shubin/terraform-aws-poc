46220abbe536b177e28fe704e3355861ede136ca
Georgiy Shubin <georgiy.sh@hotmail.com>
2019-11-30 18:26:18 +0100

    Comments prepared for Jenkins

--- jenkins/output.tf
+++ jenkins/output.tf
@@ -1,6 +1,6 @@
 output "ssh_connection" {
-  # value = join("", ["ssh -i \"", aws_instance.gs-instnace.key_name, "\" ubuntu@", aws_instance.gs-instnace.public_dns])
   value = format("ssh -i \"%s\" ubuntu@%s", aws_instance.jenkins-instnace.key_name, aws_instance.jenkins-instnace.public_dns)
+  # value = format("ssh -i \"%s\" ubuntu@%s", aws_instance.jenkins-instnace.key_name, aws_route53_record.jenkins.name)
 }
 
 output "jenkins_address" {
--- jenkins/variables.tf
+++ jenkins/variables.tf
@@ -1,24 +1,13 @@
-
-############################
-####  Common Variables  ####
-############################
-
-# Tags
 variable "tag_name" {
   type        = string
   description = "The tag, visible in the NAME column"
 }
 
-# Terraform version
 variable "tfversion" {
   type        = string
   description = "Terraform Version"
-  default     = "0.12.15"
+  default     = "0.12.16"
 }
-
-#####################
-####  Providers  ####
-#####################
 
 variable "aws_region" {
   type        = string
@@ -31,47 +20,33 @@ variable "aws_profile" {
   default     = "default"
 }
 
-###################
-####  Network  ####
-###################
-
-# Network
 variable "vpc_cidr" {
   type        = string
   description = "The CIDR Block for entire VPC"
 }
 
-# Security Group
 variable "ingress_ports" {
   type        = list(number)
   description = "Network ports"
+  default     = [22, 8080]
 }
 
 variable "protocol" {
   type        = string
   description = "Network protocol"
+  default     = "TCP"
 }
-
-###################
-####  Route53  ####
-###################
 
 variable "domain_name" {
   type        = string
   description = "Registered domain name"
 }
 
-##############
-#### EC2 #####
-##############
-
-# EC2 Key Pairs
 variable "key_pair" {
   type        = string
   description = "Key Pair to connect to an EC2 instance"
 }
 
-# EC2 Instance
 variable "instance_type" {
   type        = string
   description = "Flavor t2.micro, t2.small"
